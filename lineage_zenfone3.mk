592ed3e51370078894c9c67d851a3a84288a57d9
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2023-05-12 10:26:34 -0300

    Update lineage_zenfone3.mk

--- lineage_zenfone3.mk
+++ lineage_zenfone3.mk
@@ -20,16 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from zenfone3 device
-$(call inherit-product, device/asus/zenfone3/device.mk)
+$(call inherit-product, device/asus/A001D/device.mk)
 
 # Inherit from zenfone3 vendor
-$(call inherit-product-if-exists, vendor/asus/zenfone3/zenfone3-vendor.mk)
+$(call inherit-product-if-exists, vendor/asus/A001D/A001D-vendor.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_zenfone3
-PRODUCT_DEVICE := zenfone3
+PRODUCT_NAME := lineage_A001D
+PRODUCT_DEVICE := A001D
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 
