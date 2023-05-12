2afa7fc6d3037c9263bf5a46429bdbec4f620a53
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2023-05-12 10:29:46 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_zenfone3.mk
+    $(LOCAL_DIR)/lineage_A001D.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_zenfone3-user \
-    lineage_zenfone3-userdebug \
-    lineage_zenfone3-eng
+    lineage_A001D-user \
+    lineage_A001D-userdebug \
+    lineage_A001D-eng
